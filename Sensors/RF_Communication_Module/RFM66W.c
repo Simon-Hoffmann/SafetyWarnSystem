a485a6bea0069dd72f439ef37b85c1da6481233f
Simon Hoffmann <63074808+Simon-Hoffmann@users.noreply.github.com>
2022-11-10 13:23:58 +0100

    RFM66W files added

---
+++ Sensors/RF_Communication_Module/RFM66W.c
@@ -0,0 +1,25 @@
+/**
+*	@file 	RFM66W.c
+*	@author Simon Hoffmann
+*	@brief	RFM66W 868Mhz radio module implementation
+* @date   10.11.2022
+*/
+
+/*-------------------------- I N C L U D E S ----------------------------*/
+
+/* ----------- V A R I A B L E S   &  C O N S T A N T S  --------------- */
+
+/* ------------- F u n c t i o n  P r o t o t y p e s  ----------------- */
+
+/* ----------------------- F U N C T I O N S  -------------------------- */
+
+/**
+*	Initialisation of RFM66W Module
+*
+*	@details	
+*	@param 	none
+*	@return none
+*/
+void rfm66w_init(void){
+
+}
---
+++ Sensors/RF_Communication_Module/RF_Communication_Module.ino
@@ -0,0 +1,38 @@
+/**
+*	@file 	RF_Communication_Module.ino
+*	@author Simon Hoffmann
+*	@brief	Main Program start and loop
+* @date   10.11.2022
+*/
+
+/*-------------------------- I N C L U D E S ----------------------------*/
+
+#include "RFM66W.h"
+
+/* ----------- V A R I A B L E S   &  C O N S T A N T S  --------------- */
+
+#VERSION  "1.0.0"
+
+/* ------------- F u n c t i o n  P r o t o t y p e s  ----------------- */
+
+/* ----------------------- F U N C T I O N S  -------------------------- */
+
+/**
+*	Startup for initialisation
+*
+*	@param 	none
+*	@return none
+*/
+void setup(void) {
+
+}
+
+/**
+*	Main program loop
+*
+*	@param 	none
+*	@return none
+*/
+void loop(void) {
+
+}
