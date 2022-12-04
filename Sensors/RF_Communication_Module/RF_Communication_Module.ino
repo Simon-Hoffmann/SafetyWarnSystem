acc694efc4f042e0507525518bb66b5bf086bc1e
Simon Hoffmann <63074808+Simon-Hoffmann@users.noreply.github.com>
2022-12-04 12:42:18 +0100

    FreeRTOS added

--- Sensors/RF_Communication_Module/RF_Communication_Module.ino
+++ Sensors/RF_Communication_Module/RF_Communication_Module.ino
@@ -1,19 +1,19 @@
 /**
 *	@file 	RF_Communication_Module.ino
 *	@author Simon Hoffmann
-*	@brief	Main Program start and loop
+*	@brief	Main Program start and loop - Select the type os sensor in sensor.h
 * @date   10.11.2022
 */
 
 /*-------------------------- I N C L U D E S ----------------------------*/
 
-#include "rfm.h"
+#include <FreeRTOS.h>
+#include "sensor.h"
 
 
 /* ----------- V A R I A B L E S   &  C O N S T A N T S  --------------- */
 
 #define VERSION  "1.0.0"
-
 /* ------------- F u n c t i o n  P r o t o t y p e s  ----------------- */
 
 /* ----------------------- F U N C T I O N S  -------------------------- */
@@ -25,18 +25,19 @@
 *	@return none
 */
 void setup(void) {
-  
+  sensor_init();
 }
 
 /**
-*	Main program loop
+*	Main program loop not used because of FreeRTOS
 *
 *	@param 	none
 *	@return none
 */
-void loop(void) {
+void loop(void) {}
 
-}
+
+
 
 
 
