a7febd74fcdc76223bafd5abd4011bf848216cac
Simon Hoffmann <63074808+Simon-Hoffmann@users.noreply.github.com>
2022-12-04 12:41:04 +0100

    led added

---
+++ Sensors/RF_Communication_Module/led.h
@@ -0,0 +1,22 @@
+/**
+*	@file 	led.h
+*	@author Simon Hoffmann
+*	@brief	LED Interface
+*/
+
+#ifndef __LED_H_
+#define __LED_H_
+
+/*-------------------------- I N C L U D E S ----------------------------*/
+
+/*---------------------------- M A C R O S ------------------------------*/
+
+/* ----------- V A R I A B L E S   &  C O N S T A N T S  --------------- */
+
+/* ----------------------- F U N C T I O N S  -------------------------- */
+
+void led_init(void);
+
+void led_gas_indication(void);
+
+#endif
